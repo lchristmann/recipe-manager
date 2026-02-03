7299589fa386216c9dd43bce22fb04177fcee89b
Leander Christmann <leander.christmann@proton.me>
2026-02-03 18:42:49 +0100

    feat: finish recipe page heading partial

--- resources/views/partials/recipe-heading.blade.php
+++ resources/views/partials/recipe-heading.blade.php
@@ -8,16 +8,50 @@
 
     @can('update', $recipe)
         <div class="flex-shrink-0 ml-4">
-            {{-- Mobile: icon only --}}
-            <div class="md:hidden">
-                <flux:button :href="route('recipes.edit', ['recipe' => $recipe->id])" icon="pencil" />
+            {{-- -------------------- MOBILE -------------------- --}}
+            <div class="flex flex-col gap-2 md:hidden">
+                {{-- Edit --}}
+                <flux:button :href="route('recipes.edit', ['recipe' => $recipe->id])" icon="pencil"/>
+
+                {{-- Copy link --}}
+                <div x-data="{ copied: false }">
+                    <flux:tooltip toggleable position="left">
+                        <flux:button icon="link"
+                            @click="
+                                navigator.clipboard.writeText(window.location.href);
+                                copied = true;
+                                setTimeout(() => copied = false, 1500);
+                            "
+                        />
+
+                        <flux:tooltip.content x-show="copied">{{ __('Copied') }}</flux:tooltip.content>
+                    </flux:tooltip>
+                </div>
             </div>
 
-            {{-- Desktop: icon + text --}}
-            <div class="hidden md:block">
+            {{-- -------------------- DESKTOP -------------------- --}}
+            <div class="hidden md:flex items-center gap-2">
+                {{-- Edit --}}
                 <flux:button :href="route('recipes.edit', ['recipe' => $recipe->id])" icon="pencil">
                     {{ __('Edit recipe') }}
                 </flux:button>
+
+                {{-- Copy link --}}
+                <div x-data="{ copied: false }">
+                    <flux:tooltip toggleable position="bottom">
+                        <flux:button icon="link"
+                            @click="
+                                navigator.clipboard.writeText(window.location.href);
+                                copied = true;
+                                setTimeout(() => copied = false, 1500);
+                            "
+                        >
+                            {{ __('Copy link') }}
+                        </flux:button>
+
+                        <flux:tooltip.content x-show="copied">{{ __('Copied') }}</flux:tooltip.content>
+                    </flux:tooltip>
+                </div>
             </div>
         </div>
     @endcan
