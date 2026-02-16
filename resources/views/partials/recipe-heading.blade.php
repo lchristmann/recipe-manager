ef12d9c78715bd3f2d13b0da26075b62a0ec6645
Leander Christmann <leander.christmann@proton.me>
2026-02-16 21:59:13 +0100

    fix: copy link button shall always be shown on recipe.show page

--- resources/views/partials/recipe-heading.blade.php
+++ resources/views/partials/recipe-heading.blade.php
@@ -6,53 +6,55 @@
         @endif
     </div>
 
-    @can('update', $recipe)
-        <div class="flex-shrink-0 ml-4">
-            {{-- -------------------- MOBILE -------------------- --}}
-            <div class="flex flex-col gap-2 md:hidden">
-                {{-- Edit --}}
+    <div class="flex-shrink-0 ml-4">
+        {{-- -------------------- MOBILE -------------------- --}}
+        <div class="flex flex-col gap-2 md:hidden">
+            {{-- Edit --}}
+            @can('update', $recipe)
                 <flux:button :href="route('recipes.edit', ['recipe' => $recipe->id])" icon="pencil"/>
+            @endcan
 
-                {{-- Copy link --}}
-                <div x-data="{ copied: false }">
-                    <flux:tooltip toggleable position="left">
-                        <flux:button icon="link"
-                            @click="
-                                navigator.clipboard.writeText(window.location.href);
-                                copied = true;
-                                setTimeout(() => copied = false, 1500);
-                            "
-                        />
+            {{-- Copy link --}}
+            <div x-data="{ copied: false }">
+                <flux:tooltip toggleable position="left">
+                    <flux:button icon="link"
+                        @click="
+                            navigator.clipboard.writeText(window.location.href);
+                            copied = true;
+                            setTimeout(() => copied = false, 1500);
+                        "
+                    />
 
-                        <flux:tooltip.content x-show="copied">{{ __('Copied') }}</flux:tooltip.content>
-                    </flux:tooltip>
-                </div>
+                    <flux:tooltip.content x-show="copied">{{ __('Copied') }}</flux:tooltip.content>
+                </flux:tooltip>
             </div>
+        </div>
 
-            {{-- -------------------- DESKTOP -------------------- --}}
-            <div class="hidden md:flex items-center gap-2">
-                {{-- Edit --}}
+        {{-- -------------------- DESKTOP -------------------- --}}
+        <div class="hidden md:flex items-center gap-2">
+            {{-- Edit --}}
+            @can('update', $recipe)
                 <flux:button :href="route('recipes.edit', ['recipe' => $recipe->id])" icon="pencil">
                     {{ __('Edit recipe') }}
                 </flux:button>
+            @endcan
 
-                {{-- Copy link --}}
-                <div x-data="{ copied: false }">
-                    <flux:tooltip toggleable position="bottom">
-                        <flux:button icon="link"
-                            @click="
-                                navigator.clipboard.writeText(window.location.href);
-                                copied = true;
-                                setTimeout(() => copied = false, 1500);
-                            "
-                        >
-                            {{ __('Copy link') }}
-                        </flux:button>
+            {{-- Copy link --}}
+            <div x-data="{ copied: false }">
+                <flux:tooltip toggleable position="bottom">
+                    <flux:button icon="link"
+                        @click="
+                            navigator.clipboard.writeText(window.location.href);
+                            copied = true;
+                            setTimeout(() => copied = false, 1500);
+                        "
+                    >
+                        {{ __('Copy link') }}
+                    </flux:button>
 
-                        <flux:tooltip.content x-show="copied">{{ __('Copied') }}</flux:tooltip.content>
-                    </flux:tooltip>
-                </div>
+                    <flux:tooltip.content x-show="copied">{{ __('Copied') }}</flux:tooltip.content>
+                </flux:tooltip>
             </div>
         </div>
-    @endcan
+    </div>
 </div>
